11938c0e2653abc84222ee736c81bfb8411d7640
BT_1N3_21 <muyunz@nycstudents.net>
2024-05-30 09:11:18 -0400

    didnt do shit

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -7,19 +7,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class GraphicsPanel extends JPanel {
+public class GraphicsPanel extends JPanel{
     private BufferedImage background;
     private boolean[] pressedKeys;
     private int boardLen;
     private int boardWid;
     int blockSize = 25;
-
     private Block food;
-
     ArrayList<Block> body;
-
-
-
     Block snake;
 
     private class Block{
@@ -45,7 +40,6 @@ public class GraphicsPanel extends JPanel {
         setBackground(Color.black);
         setPreferredSize(new Dimension(boardLen, boardWid));
         snake = new Block(5, 5);
-
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);  // just do this
@@ -61,4 +55,6 @@ public class GraphicsPanel extends JPanel {
        g.fillRect(snake.x * blockSize, snake.y * blockSize, blockSize,blockSize);
 
     }
+
+
 }
